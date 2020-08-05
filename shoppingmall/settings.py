1137bc86cf75e043567a7c2320dbee8ac3a745d2
SJLEE316 <lsj8249@naver.com>
2020-08-05 20:19:31 +0900

    base & navbar

--- shoppingmall/settings.py
+++ shoppingmall/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '_b@o*)02i1dwv81aa9%+f-4y9@%7khb(268*iya8^-^6sg5*yr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -105,15 +104,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
+
 
 
 # Static files (CSS, JavaScript, Images)
