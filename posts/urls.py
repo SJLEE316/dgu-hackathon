d2084e4728995d666ae419c599d81dac1c92796c
SJLEE316 <lsj8249@naver.com>
2020-08-11 15:05:18 +0900

    READ show edit

--- posts/urls.py
+++ posts/urls.py
@@ -6,9 +6,6 @@ urlpatterns = [
     path('new/', new, name="new"),
     path('create/', create, name="create"),
     path('main/', main, name="main"),
-    path('main2/', main2, name="main2"),
-    path('main3/', main2, name="main3"),
-    path('main4/', main2, name="main4"),
-    path('main5/', main2, name="main5"),    
+    
     path('show/<int:id>', show, name="show"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -15,23 +15,6 @@ def main(request):
     posts = Post.objects.all()
     return render(request, 'posts/main.html', {'posts': posts})   
 
-def main2(request):
-    posts = Post.objects.all()
-    return render(request, 'posts/main2.html', {'posts': posts})   
-    
-def main3(request):
-    posts = Post.objects.all()
-    return render(request, 'posts/main3.html', {'posts': posts})   
-    
-def main4(request):
-    posts = Post.objects.all()
-    return render(request, 'posts/main4.html', {'posts': posts})   
-
-def main5(request):
-    posts = Post.objects.all()
-    return render(request, 'posts/main5.html', {'posts': posts})   
-
-
 def show(request, id):
     post = Post.objects.get(pk=id)
-    return render(request, 'posts/show.html', {'posts': post})
+    return render(request, 'posts/show.html', {'post': post})
